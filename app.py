7228839cc4a1e137529cf537578b7c0defd16f63
Renat <testinggame778@gmail.com>
2023-04-15 09:56:47 +0300

    v 1.7 updated requirements.txt

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-from flask import Flask
+from datetime import datetime
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    from datetime import datetime
-    print(datetime.now())
-
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+print(datetime.now())
